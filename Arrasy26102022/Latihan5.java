0547c9c75d8d2803b41e2eaa9234eeb3ecff6a5f
arrasy2107 <arrasyrtaher21@gmail.com>
2022-12-01 21:57:12 +0700

    Java Array
    Pertemuan 26 Oktober 2022

---
+++ Arrasy26102022/Latihan5.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Arrasy26102022;
+
+/**
+ *
+ * @author USER
+ */
+public class Latihan5 {
+    public static void main(String[] args) {
+        System.out.println("Number of arguments=" + args.length);
+        System.out.println("First Argument=" + args[3]);
+    }
+}
