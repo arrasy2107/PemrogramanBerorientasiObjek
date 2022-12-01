8abc1fa2e76ad2209f03a447ecdf6b1b8913d09c
arrasy2107 <arrasyrtaher21@gmail.com>
2022-12-01 21:59:45 +0700

    For - Do - While
    Pertemuan 4 November 2022

---
+++ Arrasy04112022/Latihan1DoWhile.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Arrasy04112022;
+
+/**
+ *
+ * @author USER
+ */
+public class Latihan1DoWhile {
+    public static void main(String[]args)
+    {
+        String[] hari = { "Senin","Selasa","Rabu","Kamis","Jumat","Sabtu","Minggu"};
+
+        int i=0;
+        do
+            {
+                System.out.println(hari[i]);
+                i++;
+            }while(i<hari.length);
+    }
+}
+
---
+++ Arrasy04112022/Latihan1For.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Arrasy04112022;
+
+/**
+ *
+ * @author USER
+ */
+public class Latihan1For {
+    public static void main(String[]args)
+    {
+        String[] hari = { "Senin","Selasa","Rabu","Kamis","Jumat","Sabtu","Minggu"};
+        for(int i=0;i<hari.length;i++)
+            {
+                System.out.println(hari[i]);
+            }
+    }
+}
